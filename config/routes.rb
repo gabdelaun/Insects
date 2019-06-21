5b60caecc18303e8f208c8956efa4d78b879faad
gabdelaun <gabdelaun@gmail.com>
2019-06-21 13:27:44 +0200

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  root 'welcome#index'
 
   get 'pages/contact'
+  root 'pages#contact'
 
   get 'pages/about'
-
+  root 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
